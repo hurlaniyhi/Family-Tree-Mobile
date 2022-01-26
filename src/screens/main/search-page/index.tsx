5b085c7028eec752d4b4ec51473e303c21200772
Ridwan Kolawole <olaniyi.jibola152@gmail.com>
2022-01-26 13:24:46 +0100

    make it better

--- src/screens/main/search-page/index.tsx
+++ src/screens/main/search-page/index.tsx
@@ -62,18 +62,18 @@ export const SearchPage: React.FC<any> = () => {
                 <ScrollView>
                     <Card cardPadding="10"cardTopMargin="10" >
                         <SearchedDataContainer>
-                            <DataPictureWrapper customWidth="90" flexBottomMargin="0">
-                                <FamDataPicture customWidth="90" source={icons.DP3} />
+                            <DataPictureWrapper customWidth="80" flexBottomMargin="0">
+                                <FamDataPicture customWidth="80" source={icons.DP3} />
                             </DataPictureWrapper>
                             <ActionInfoWrapper>
-                                <AppText fontSize="18" fontWeight="900">Kolawole Family</AppText>
-                                <AppText fontSize="14" textColor="#A6A6A6" fontWeight="500">10 Members</AppText>
+                                <AppText fontSize="16" fontWeight="900">Kolawole Family</AppText>
+                                <AppText fontSize="12" textColor="#A6A6A6" fontWeight="500">10 Members</AppText>
                                 <ActionButtonWrapper>
                                     <FillButton>
-                                        <AppText fontSize="13" textColor="#FFFFFF">Set as Family</AppText>
+                                        <AppText fontSize="12" textColor="#FFFFFF">Set as Family</AppText>
                                     </FillButton>
                                     <OutlineButton>
-                                        <AppText fontSize="13" textColor={theme.FOCUS_THEME_COLOR}>View Members</AppText>
+                                        <AppText fontSize="12" textColor={theme.FOCUS_THEME_COLOR}>View Members</AppText>
                                     </OutlineButton>
                                 </ActionButtonWrapper >
                             </ActionInfoWrapper>
@@ -81,18 +81,18 @@ export const SearchPage: React.FC<any> = () => {
                     </Card>
                     <Card cardPadding="10" cardTopMargin="10">
                         <SearchedDataContainer>
-                            <DataPictureWrapper customWidth="90" flexBottomMargin="0">
-                                <FamDataPicture customWidth="90" source={icons.DP4} />
+                            <DataPictureWrapper customWidth="80" flexBottomMargin="0">
+                                <FamDataPicture customWidth="80" source={icons.DP4} />
                             </DataPictureWrapper>
                             <ActionInfoWrapper>
-                                <AppText fontSize="18" fontWeight="900">Aweda Family</AppText>
-                                <AppText fontSize="14" textColor="#A6A6A6" fontWeight="500">15 Members</AppText>
+                                <AppText fontSize="16" fontWeight="900">Aweda Family</AppText>
+                                <AppText fontSize="12" textColor="#A6A6A6" fontWeight="500">15 Members</AppText>
                                 <ActionButtonWrapper>
                                     <FillButton>
-                                        <AppText fontSize="13" textColor="#FFFFFF">Set as Family</AppText>
+                                        <AppText fontSize="12" textColor="#FFFFFF">Set as Family</AppText>
                                     </FillButton>
                                     <OutlineButton>
-                                        <AppText fontSize="13" textColor={theme.FOCUS_THEME_COLOR}>View Members</AppText>
+                                        <AppText fontSize="12" textColor={theme.FOCUS_THEME_COLOR}>View Members</AppText>
                                     </OutlineButton>
                                 </ActionButtonWrapper >
                             </ActionInfoWrapper>
@@ -100,18 +100,18 @@ export const SearchPage: React.FC<any> = () => {
                     </Card>
                     <Card cardPadding="10" cardTopMargin="10">
                         <SearchedDataContainer>
-                            <DataPictureWrapper customWidth="90" flexBottomMargin="0">
-                                <FamDataPicture customWidth="90" source={icons.DP2} />
+                            <DataPictureWrapper customWidth="80" flexBottomMargin="0">
+                                <FamDataPicture customWidth="80" source={icons.DP2} />
                             </DataPictureWrapper>
                             <ActionInfoWrapper>
-                                <AppText fontSize="18" fontWeight="900">Olanrewaju Family</AppText>
-                                <AppText fontSize="14" textColor="#A6A6A6" fontWeight="500">8 Members</AppText>
+                                <AppText fontSize="16" fontWeight="900">Olanrewaju Family</AppText>
+                                <AppText fontSize="12" textColor="#A6A6A6" fontWeight="500">8 Members</AppText>
                                 <ActionButtonWrapper>
                                     <FillButton>
-                                        <AppText fontSize="13" textColor="#FFFFFF">Set as Family</AppText>
+                                        <AppText fontSize="12" textColor="#FFFFFF">Set as Family</AppText>
                                     </FillButton>
                                     <OutlineButton>
-                                        <AppText fontSize="13" textColor={theme.FOCUS_THEME_COLOR}>View Members</AppText>
+                                        <AppText fontSize="12" textColor={theme.FOCUS_THEME_COLOR}>View Members</AppText>
                                     </OutlineButton>
                                 </ActionButtonWrapper >
                             </ActionInfoWrapper>
@@ -119,18 +119,18 @@ export const SearchPage: React.FC<any> = () => {
                     </Card>
                     <Card cardPadding="10" cardTopMargin="10">
                         <SearchedDataContainer>
-                            <DataPictureWrapper customWidth="90" flexBottomMargin="0">
-                                <FamDataPicture customWidth="90" source={icons.DP} />
+                            <DataPictureWrapper customWidth="80" flexBottomMargin="0">
+                                <FamDataPicture customWidth="80" source={icons.DP} />
                             </DataPictureWrapper>
                             <ActionInfoWrapper>
-                                <AppText fontSize="18" fontWeight="900">Ajulibe Family</AppText>
-                                <AppText fontSize="14" textColor="#A6A6A6" fontWeight="500">6 Members</AppText>
+                                <AppText fontSize="16" fontWeight="900">Ajulibe Family</AppText>
+                                <AppText fontSize="12" textColor="#A6A6A6" fontWeight="500">6 Members</AppText>
                                 <ActionButtonWrapper>
                                     <FillButton>
-                                        <AppText fontSize="13" textColor="#FFFFFF">Set as Family</AppText>
+                                        <AppText fontSize="12" textColor="#FFFFFF">Set as Family</AppText>
                                     </FillButton>
                                     <OutlineButton>
-                                        <AppText fontSize="13" textColor={theme.FOCUS_THEME_COLOR}>View Members</AppText>
+                                        <AppText fontSize="12" textColor={theme.FOCUS_THEME_COLOR}>View Members</AppText>
                                     </OutlineButton>
                                 </ActionButtonWrapper >
                             </ActionInfoWrapper>
